acb2d2bc0fefae55e42cc4d7d17234dfb62f274a
Anastasia Malgina <malgina.an@gmail.com>
2024-03-10 22:47:46 +0300

    test

--- tests/test_registrationion.py
+++ tests/test_registrationion.py
@@ -1,23 +1,33 @@
-from selenium import webdriver
-import time
-
-# инициализируем драйвер браузера
-driver = webdriver.Chrome()
-driver.get("https://stellarburgers.nomoreparties.site/")
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from locators import Locators
+from conftyest import driver, generate_random_username, generate_random_password, generate_random_email
 
 
+class TestRegistration:
+    def test_registration_positive(self, driver, generate_random_username,
+                                   generate_random_password, generate_random_email):
+        driver.find_element(*Locators.BUTTON_PERSONAL_ACCOUNT).click()
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(Locators.LINK_TO_FORM_REGISTRATION)).click()
+        driver.find_element(*Locators.NAME).send_keys(generate_random_username)
+        driver.find_element(*Locators.EMAIL).send_keys(generate_random_email)
+        driver.find_element(*Locators.PASSWORD).send_keys(generate_random_password)
+        driver.find_element(*Locators.BUTTON_REGISTRATION).click()
 
-# страница логинации
-# https://stellarburgers.nomoreparties.site/login
+        driver.quit()
 
-# Регистрация
-# Проверь:
-# Успешную регистрацию.
-# 1 Поле «Имя» должно быть не пустым;
-# 2 в поле Email введён email в формате логин@домен: например, 123@ya.ru.
-# 3 Минимальный пароль — шесть символов.
-# 4 Ошибку для некорректного пароля.
+    def test_registration_negative(self, driver, generate_random_username,
+                                   generate_random_password, generate_random_email):
+        driver.find_element(*Locators.BUTTON_PERSONAL_ACCOUNT).click()
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(Locators.LINK_TO_FORM_REGISTRATION)).click()
+        driver.find_element(*Locators.NAME).send_keys(generate_random_username)
+        driver.find_element(*Locators.EMAIL).send_keys(generate_random_email)
+        driver.find_element(*Locators.PASSWORD).send_keys('54321')
+        driver.find_element(*Locators.BUTTON_REGISTRATION).click()
+        incorrect_password = WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(Locators.ERROR_INCORRECT_PASSWORD)).text
+        assert incorrect_password == 'Некорректный пароль'
 
-
-# закроем браузер
-driver.quit()
+        driver.quit()
